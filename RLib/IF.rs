09ffa6b942264c42fef253c6598c4bf78caf9f16
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-04-16 22:02:35 +0200

    Create IF.rs

---
+++ RLib/IF.rs
@@ -0,0 +1,22 @@
+fn main()
+{
+
+    let x: f64 = 9.1;
+    let b: bool = true;
+
+    println!("FLOAT, BOOL: {}, {}", x, b);
+
+    if x > 10.0
+    {
+
+        println!(" X > 10");
+    }else if x == 10.0
+    {
+        println!(" X == 10");
+    }else
+    {
+
+        println!(" X < 10");
+    }
+
+}
