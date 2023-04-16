bcb9c0d216dd117b291e6c1670ce50bff29b3d3f
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-04-16 20:47:49 +0200

    Create start_ith_rust.rs

---
+++ start_ith_rust.rs
@@ -0,0 +1,20 @@
+const NUMBER1: i32 = 8;
+
+fn main() 
+{
+    
+    // ___
+    let number = "T-H-R-E-E";
+    println!("Number_1: {}", number);
+    
+    let number: i32 = 3;
+    println!("Number_1.1: {}", number);
+    
+    println!("Sum_7and_Number: {}", number + 7);
+    
+    // ___
+    
+    println!("Const_Number: {}, {}", NUMBER1, NUMBER2);
+}
+
+const NUMBER2: i32 = 4;
